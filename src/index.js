4a2a1729af4905abd31a8602682c592cbd54c10f
thanhpx1 <thanhpx1@greenglobal.vn>
2016-10-06 08:02:49 +0700

    fix style

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-var isPrime = function (num) {
-	for (var i = 2; i <= num; i++) {
-		if (num % 2 == 0) {
-			return false;
-		}
-	}
-	return true;
+var isPrime = function isPrime(num) {
+  for (var i = 2; i <= num; i++) {
+    if (num % 2 === 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
-var sumPrimes = function (num, callback) {
-	if (isNaN(num)) {
-		var err = new Error('Param must be a number');
-		return callback(err);
-	}
+var sumPrimes = function sumPrimes(num, callback) {
+  if (isNaN(num)) {
+    return callback(Error('Param must be a number'));
+  }
 
-	if (num < 0) {
-		var err = new Error('Param must be > 0');
-		return callback(err);
-	}
+  if (num < 0) {
+    var err = new Error('Param must be > 0');
+    return callback(err);
+  }
 
-	var sum = 0;
-	for (var i = 0;  i < num; i++) {
-		if (isPrime(i) == true) {
-			sum += i;
-		}
-	}
+  var sum = 0;
+  for (var i = 0; i < num; i++) {
+    if (isPrime(i) === true) {
+      sum += i;
+    }
+  }
 
-	var s = sum;
-	callback(null, s);
+  var s = sum;
+  return callback(null, s);
 };
 
 module.exports = {
-	sumPrimes : sumPrimes
-}
+  sumPrimes
+};
